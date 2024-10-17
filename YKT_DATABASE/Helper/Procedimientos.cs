86c90a74ca4eec44943b6b8990012f756477296e
Carlos-AB <66312231+Carlos-AB@users.noreply.github.com>
2024-10-16 21:09:04 -0500

    a

---
+++ YKT_DATABASE/Helper/Procedimientos.cs
@@ -0,0 +1,19 @@
+﻿namespace YKT.DATABASE.Helper
+{
+    public static class Procedimientos
+    {
+     
+        public static class SEGURIDAD
+        {
+            public const string CrudRegister = "SEG.USP_CRUD_USUARIOS";
+            public const string CrudCombos = "SEG.USP_CRUD_COMBOS";
+            public const string GrupoDatoCrud = "SEG.USP_CRUD_MSTB05";
+        }
+
+
+        public static class ADMIN {
+            public const string CrudRestaurant = "ADM.USP_CRUD_RESTAURANT";
+        }
+
+    }
+}
